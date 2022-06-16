90ded333f9d9c587b19da368aaaacc84def629b0
Oshin Gansi <os.gansi@gmail.com>
2022-06-16 23:20:28 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3333, () => {
+  console.log("testing");
+});
